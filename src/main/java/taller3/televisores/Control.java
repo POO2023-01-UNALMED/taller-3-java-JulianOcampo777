c61d5bb5c8fd9b94795991098062c04224e71a17
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-02-27 18:56:57 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,55 +1,37 @@
 package taller3.televisores;
 
-public class Control {
-	
+	public class Control {
 	private TV tv;
-	
-	public void enlazar(TV tv) {
-		
-		this.tv = tv;
-		tv.setControl(this);
-		
-	}
-	
-	public TV getTv() {
-		return tv;
-	}
-	
 	public void setTv(TV tv) {
-		this.tv = tv;
+		this.tv=tv;
 	}
-	
+	public TV getTv() {
+		return this.tv;
+	}
 	public void turnOn() {
 		tv.turnOn();
 	}
-	
 	public void turnOff() {
 		tv.turnOff();
 	}
-	
 	public void canalUp() {
 		tv.canalUp();
 	}
-	
 	public void canalDown() {
 		tv.canalDown();
 	}
-	
 	public void volumenUp() {
 		tv.volumenUp();
 	}
-	
 	public void volumenDown() {
 		tv.volumenDown();
 	}
-	
 	public void setCanal(int canal) {
 		tv.setCanal(canal);
 	}
-	
-	public void setVolumen(int volumen) {
-		tv.setVolumen(volumen);
+	public void enlazar(TV tv) {
+		this.tv=tv;
+		tv.setControl(this);
 	}
-	
 
 }
