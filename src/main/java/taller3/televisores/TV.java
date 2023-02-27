f75b67f619b37f6d1d097c157a059079171d48e9
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-02-27 18:32:09 -0500

    t2

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -4,10 +4,10 @@ public class TV {
     private Marca marca;
     private int canal=1;
     private int precio=500;
-    boolean estado;
+    private boolean estado;
     private int volumen=1;
     private Control control;
-    static int numTV; 
+    private static int numTV; 
 
     public TV(Marca marca, boolean estado) {
         this.marca=marca;
