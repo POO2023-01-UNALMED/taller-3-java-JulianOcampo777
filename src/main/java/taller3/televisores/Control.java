2fc50f1995d2bfc10863c6954224d70b10184152
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-02-27 18:42:52 -0500

    t5

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,59 +1,55 @@
 package taller3.televisores;
 
 public class Control {
-    TV tv;
+	
+	private TV tv;
+	
+	public void enlazar(TV tv) {
+		
+		this.tv = tv;
+		tv.setControl(this);
+		
+	}
+	
+	public TV getTv() {
+		return tv;
+	}
+	
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+	
+	public void turnOn() {
+		tv.turnOn();
+	}
+	
+	public void turnOff() {
+		tv.turnOff();
+	}
+	
+	public void canalUp() {
+		tv.canalUp();
+	}
+	
+	public void canalDown() {
+		tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+	
+	public void setVolumen(int volumen) {
+		tv.setVolumen(volumen);
+	}
+	
 
-    public void enlazar(TV tv) {
-        tv.setControl(this);
-        this.tv=tv;
-    }
-
-    public TV getTv() {
-        return tv;
-    }
-    public void setTv(TV tv) {
-        this.tv = tv;
-    }
-
-    public void turnOn() {
-        tv.turnOn();
-    }
-    public void turnOff() {
-        tv.turnOff();
-    }
-
-    public void canalUp() {
-        if (tv.getEstado()==true) {
-            if (tv.getCanal()>=1 && tv.getCanal()<120) {
-                tv.setCanal(tv.getCanal()+1);
-            }
-        }     
-    }
-    
-    public void canalDown() {
-        if (tv.getEstado()==true) {
-            if (tv.getCanal()>1 && tv.getCanal()<=120) {
-                tv.setCanal(tv.getCanal()-1);
-            }
-        }
-    }
-
-    public void volumenUp() {
-        if (tv.getEstado()==true) {
-            if (tv.getVolumen()>=0 && tv.getVolumen()<7) {
-                tv.setVolumen(tv.getVolumen()+1);
-            }
-        }
-    }
-    
-    public void volumenDown() {
-        if (tv.getEstado()==true) {
-            if (tv.getVolumen()>0 && tv.getVolumen()<=7) {
-                tv.setVolumen(tv.getVolumen()-1);
-            }
-        }
-    }
-    
-    public void setCanal(int canal) {
-    }
 }
